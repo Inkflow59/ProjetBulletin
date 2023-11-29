244ae4a170271b0838a28f4646d2a6c19c989c23
Inkflow <101215221+Inkflow59@users.noreply.github.com>
2023-11-29 12:14:44 +0100

    Création de la fonction tableauLignes()

--- js/affichageNotes.js
+++ js/affichageNotes.js
@@ -11,6 +11,14 @@ function nomCandidatSaisie() {
 	var nomTotal=nom+" "+prenom
 	document.getElementById("NomCandidat").innerHTML="Candidat : "+nomTotal
 }
-function tableauLignes() {}
+function tableauLignes() {
+	var tableau=document.getElementById("Tableau")
+	var taille=sessionStorage.getItem("nombreDeNotes")
+	var ligne=tableau.insertRow(-1)
+	var colonne1=ligne.insertCell(0)
+	var colonne2=ligne.insertCell(1)
+	var i=0
+	for (i; i<taille-1; i++) {}
+}
 
 function affichageNotes() {}
